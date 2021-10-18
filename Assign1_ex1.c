9b675c00adb513eeff4553c53bf8e18be665b501
KerolousAziz <91996006+KerolousAziz@users.noreply.github.com>
2021-10-18 13:28:27 +0200

    print c programming

---
+++ Assign1_ex1.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 8, 2021
+ *      Author: kerolous Aziz
+ */
+#include "stdio.h"
+#define print(...) printf(__VA_ARGS__);fflush(stdin);fflush(stdout);
+#define scan(...) scanf(__VA_ARGS__);fflush(stdin);fflush(stdout);
+int main (void)
+{
+	print("C programming\n");
+
+
+
+	return 0;
+}
